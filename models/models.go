0bcf7944407b6f163669ff4f0d91e0e93731c8c9
AsifArko <asifarko97@gmail.com>
2018-05-22 16:50:22 +0600

    extracted albumInfo , page feed and post pictures and status with captions

---
+++ models/models.go
@@ -0,0 +1,65 @@
+package models
+
+type AlbumInfo struct {
+	_Usage struct {
+		App struct {
+			CallCount    int `json:"call_count"`
+			TotalCputime int `json:"total_cputime"`
+			TotalTime    int `json:"total_time"`
+		} `json:"app"`
+		Page struct {
+			CallCount    int `json:"call_count"`
+			TotalCputime int `json:"total_cputime"`
+			TotalTime    int `json:"total_time"`
+		} `json:"page"`
+	} `json:"__usage__"`
+	Data []struct {
+		ID     string `json:"id"`
+		Name   string `json:"name"`
+		Photos struct {
+			Data []struct {
+				ID      string `json:"id"`
+				Picture string `json:"picture"`
+			} `json:"data"`
+			Paging struct {
+				Cursors struct {
+					After  string `json:"after"`
+					Before string `json:"before"`
+				} `json:"cursors"`
+			} `json:"paging"`
+		} `json:"photos"`
+	} `json:"data"`
+	Paging struct {
+		Cursors struct {
+			After  string `json:"after"`
+			Before string `json:"before"`
+		} `json:"cursors"`
+	} `json:"paging"`
+}
+
+type PageFeed struct {
+	_Usage struct {
+		App struct {
+			CallCount    int `json:"call_count"`
+			TotalCputime int `json:"total_cputime"`
+			TotalTime    int `json:"total_time"`
+		} `json:"app"`
+		Page struct {
+			CallCount    int `json:"call_count"`
+			TotalCputime int `json:"total_cputime"`
+			TotalTime    int `json:"total_time"`
+		} `json:"page"`
+	} `json:"__usage__"`
+	Data []struct {
+		CreatedTime string `json:"created_time"`
+		ID          string `json:"id"`
+		Message     string `json:"message"`
+		Story       string `json:"story"`
+	} `json:"data"`
+	Paging struct {
+		Cursors struct {
+			After  string `json:"after"`
+			Before string `json:"before"`
+		} `json:"cursors"`
+	} `json:"paging"`
+}
